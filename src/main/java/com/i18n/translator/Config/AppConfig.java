9f645bcdfa0755397d422e4fd3e09c713a64f9a2
muhammadammarmehmood <mehmood.muhammadammar@gmail.com>
2025-05-11 20:21:01 +0500

    Added api key security

--- src/main/java/com/i18n/translator/Config/AppConfig.java
+++ src/main/java/com/i18n/translator/Config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.i18n.translator.Config;
 
+import lombok.Value;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/i18n/translator/Security/APIFilter.java
@@ -0,0 +1,33 @@
+package com.i18n.translator.Security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+@Component
+public class APIFilter extends OncePerRequestFilter {
+
+    private static final String API_KEY_HEADER = "X-API-KEY";
+    @Value("${api.key}")
+    private String validApiKey; // Replace with secure key
+
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+        String apiKey = request.getHeader(API_KEY_HEADER);
+        if (!validApiKey.equals(apiKey)) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write("Unauthorized - Invalid API Key");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+}
